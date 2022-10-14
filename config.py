f0d55699d959802b84ae13d80cc84ca4c5f7e47d
Houssam Ayyache <hous_1993@hotmail.com>
2022-10-14 22:58:47 +0300

    Add time in force to config

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
-risk_reward = 1.4
-risk = 5
+risk_reward = 1.2
+risk = 10
 risk_unit = "PERCENTAGE"    # {AMOUNT, PERCENTAGE}
 
 adapt_risk_reward_to_include_fees = "no"
+
+time_in_force = "PostOnly"  # {GoodTillCancel, ImmediateOrCancel, FillOrKill, FillOrKill}
