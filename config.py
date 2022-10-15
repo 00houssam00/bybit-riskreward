6517fff06e21c82f2ec655e06d613bf805d284d2
Houssam Ayyache <hous_1993@hotmail.com>
2022-10-15 11:42:18 +0300

    hardcode time_in_force in requests

--- config.py
+++ config.py
@@ -3,5 +3,3 @@ risk = 10
 risk_unit = "PERCENTAGE"    # {AMOUNT, PERCENTAGE}
 
 adapt_risk_reward_to_include_fees = "no"
-
-time_in_force = "PostOnly"  # {GoodTillCancel, ImmediateOrCancel, FillOrKill, FillOrKill}
